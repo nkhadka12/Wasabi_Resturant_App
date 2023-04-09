8731bd13c805f11e8d4bcbc62cfebcfef9e7d0dd
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2023-04-09 11:33:45 -0500

    I
    This is my app.

---
+++ wasabiHomepage/views.py
@@ -0,0 +1,57 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import starterMenu
+
+# Create your views here.
+def home(request):
+    menus = starterMenu.objects.all()
+    return render(request,'index.html',{'menus':menus})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request,'loginWasabi.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['user-name']
+        first_name = request.POST['first-name']
+        last_name = request.POST['last-name']
+        email = request.POST['your-email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm-password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken...')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken...')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,first_name=first_name,last_name = last_name,password=password,email=email)
+                user.save();
+                print('user created')
+                return redirect('login')
+        else:
+            messages.info(request,'Password not matching...')
+            return redirect('register')
+    
+    else:
+        return render(request,'register.html')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
